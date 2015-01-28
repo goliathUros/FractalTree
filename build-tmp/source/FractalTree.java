18972b9cb81f80aaa88d3dd02591c9e211ea79a3
unknown <dayi@697-334-Lorien.sf.k12.internal>
2015-01-28 10:11:09 -0800

    done and ready

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .9; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .9f; 
 private int smallestBranch = 5; 
-private double branchAngle = .4;
+private double branchAngle = .4f;
 public void setup() 
 {   
 	size(640,480);
@@ -16,9 +32,9 @@ public void keyPressed()
 {
 	if(keyCode == UP)
 	{
-		branchAngle += 0.05;
+		branchAngle += 0.05f;
 	} else if(keyCode ==  DOWN) {
-		branchAngle -= 0.05;
+		branchAngle -= 0.05f;
 	}
 }
 public void drawBranches(int x,int y, double branchLength, double angle) 
@@ -38,3 +54,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 		drawBranches(endX2, endY2, branchLength-8, angle2-branchAngle); 
 	}
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
